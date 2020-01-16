b470849b4dfeb6e520ecbb3880935c26ade881d9
justDoIt1314 <1572368165@qq.com>
2020-01-16 16:14:10 +0800

    Update train.py
    如果准确率大于90%,保存模型,完成训练

--- train.py
+++ train.py
@@ -234,8 +234,8 @@ def train_crack_captcha_cnn():
                 batch_x_test, batch_y_test = get_next_batch(100)
                 acc = sess.run(accuracy, feed_dict={X: batch_x_test, Y: batch_y_test, keep_prob: 1.})
                 print(step, acc)
-                # 如果准确率大于50%,保存模型,完成训练
-                if acc > 0.98:
+                # 如果准确率大于90%,保存模型,完成训练
+                if acc > 0.9:
                     saver.save(sess, "model/crack_capcha.model", global_step=step)
                     break
 
